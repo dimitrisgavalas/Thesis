212cabba8566829015717725bdfae916d1494184
dimitrisgavalas <dimitrisgavalas97@gmail.com>
2023-02-14 22:38:00 +0200

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import PopularCourses from './components/PopularCourses';
+import Universities from './components/Universities';
+import Footer from './components/Footer';
+import MainPage from './pages/MainPage';
+import ECoursesPage from './pages/ECoursesPage';
+import AboutUsPage from './pages/AboutUsPage';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <SearchBar />
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/e-courses" component={ECoursesPage} />
+        <Route exact path="/about-us" component={AboutUsPage} />
+      </Switch>
+      <PopularCourses />
+      <Universities />
+      <Footer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
