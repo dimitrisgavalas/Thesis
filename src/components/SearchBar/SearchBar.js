efbd480de826d7e4d71d4b3b3301c89984e556be
dimitrisgavalas <dimitrisgavalas97@gmail.com>
2023-10-03 18:47:22 +0300

    Fix Search Links

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { AutoComplete } from "primereact/autocomplete";
 import { EcoursesData } from "../../mock-d/EcoursesData"; // Make sure this import is correct
+import { useNavigate } from "react-router-dom"; // Import useNavigate from react-router-dom
 import "./SearchBar.css";
 
 function SearchBar() {
@@ -8,6 +9,7 @@ function SearchBar() {
   const [ecourses, setEcourses] = useState([]);
   const [selectedEcourse, setSelectedEcourse] = useState(null);
   const [filteredEcourses, setFilteredEcourses] = useState(null);
+  const navigate = useNavigate(); // Get the navigate function from react-router-dom
 
   useEffect(() => {
     EcoursesData.getAllEcourses().then((data) => setEcourses(data));
@@ -32,16 +34,22 @@ function SearchBar() {
     }, 250);
   };
 
+  const onEcourseSelect = (ecourse) => {
+    // Handle ecourse selection here, for example, redirect to the ecourse page
+    navigate(`/singleEcourse/${ecourse.id}`); // Assuming you have a route like "/ecourse/:id"
+  };
+
   return (
     <div className="container flex flex-column justify-content-center align-items-center">
       <div className="card flex justify-content-center pt-3 text-base">
         <AutoComplete
-          field="title" // Change to the field you want to search by (e.g., "title")
+          field="title"
           value={selectedEcourse}
           suggestions={filteredEcourses}
           completeMethod={search}
           onChange={(e) => setSelectedEcourse(e.value)}
-          placeholder="Search"
+          placeholder="Search Ecourses"
+          onSelect={(e) => onEcourseSelect(e.value)} // Handle the selection event
         />
       </div>
     </div>
